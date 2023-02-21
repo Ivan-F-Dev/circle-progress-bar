3837e9b04c6483532c5e3dd1a7133c48e151cc93
Ivan-F-Dev <89821080113@mail.ru>
2023-02-21 17:17:34 +0400

    chart 1.1

--- src/App.tsx
+++ src/App.tsx
@@ -2,62 +2,6 @@ import React, {useState} from 'react';
 import s from './App.module.scss';
 import Chart from "./components/Chart/Chart";
 
-// ChartJS.register(ArcElement, Tooltip, Legend);
-//
-// export const options = {
-//     plugins: {
-//         legend: {
-//             display: false
-//         },
-//         tooltip: {
-//             enabled: false
-//         }
-//     }
-// }
-//
-// export const data = {
-//     labels: ['Red', 'Blue'],
-//     datasets: [
-//         {
-//             label: '1',
-//             data: [15,1],
-//             backgroundColor: [
-//                 'rgba(196, 196, 196, 1)',
-//                 'rgba(231, 231, 231, 1)'
-//             ],
-//             borderColor: [
-//                 'rgba(196, 196, 196, 1)',
-//                 'rgba(231, 231, 231, 1)'
-//             ],
-//             borderWidth: 1,
-//             circumference:270,
-//             rotation:180,
-//             //offset:200,
-//             //spacing:100,
-//             cutout: '90%',
-//             radius: '85%'
-//         },
-//         {
-//             label: '2',
-//             data: [19,2],
-//             backgroundColor: [
-//                 'rgba(255, 127, 0, 1)',
-//                 'rgba(231, 231, 231, 1)'
-//             ],
-//             borderColor: [
-//                 'rgba(255, 127, 0, 1)',
-//                 'rgba(231, 231, 231, 1)'
-//             ],
-//             borderWidth: 1,
-//             circumference:270,
-//             rotation:180,
-//             cutout: '40%'
-//             //weight:5,
-//         }
-//     ],
-//
-// };
-
 function App() {
 
     const [chartSize,setChartSize] = useState<number>(500)
